83214b54e7f29cc6ba839cd0853b66c85d40f487
jini_11 <jieun21124@naver.com>
2021-02-26 00:55:54 +0900

    20210225

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
-function App() {
-  return (
-  <div className="App">
-    Hello
-  </div>
-  );
-}
+import PropTypes from "prop-types";
+import React from "react";
 
+class App extends React.Component{
+ state={
+   isLoading:true,
+   movies:[]
+ }
+ componentDidMount(){
+   setTimeout(()=>{
+     this.setState({isLoading: false});
+   },6000);
+   
+ }
+  render(){
+    const {isLoading}=this.state;
+    return <div>{isLoading ? "Loading...":"We are ready"}</div>;
+  }
+}
 export default App;
