033dbd00622b629f502a6129edfbabf82419e638
jini_11 <jieun21124@naver.com>
2021-03-03 10:38:12 +0900

    20210303

---
+++ src/routes/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./About.css";
+
+function About(props){
+    return (
+    <span className="about__container">
+        <span>
+        "Freedom is the freedom to say that two plus two make four. If that is granted, all else follows".<br></br>
+        </span>
+       <span>-George Orwell, 1984</span>
+    </span>
+    );
+
+}
+
+export default About;
